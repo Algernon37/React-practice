78f9813f3fd96839b2e7131f192a853dde4915e2
Ivan Esin <ivan.esin.78@mail.ru>
2024-03-12 19:50:36 +0400

    Add a new hooks

--- my-second-project/src/App.js
+++ my-second-project/src/App.js
@@ -1,10 +1,12 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState } from 'react';
 import "./styles/App.css";
 import PostList from './components/PostList';
 import PostForm from './components/PostForm';
 import PostFilter from './components/PostFilter';
 import MyModal from './components/UI/MyModal/MyModal';
 import MyButton from './components/UI/button/MyButton';
+import { usePosts } from './components/hooks/usePosts';
+import axios from 'axios';
 
 function App() {
   const [posts, setPosts] = useState([
@@ -15,24 +17,17 @@ function App() {
 
   const [filter, setFilter] = useState({ sort: '', query: '' });
   const [modal, setModal] = useState(false);
-
-
-  const sortedPosts = useMemo(() => {
-    if (filter.sort) {
-      return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]))
-    }
-    return posts
-  }, [filter.sort, posts])
-
-
-  const sortedAndSearchedPosts = useMemo(() => {
-    return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query))
-  }, [filter.query, sortedPosts])
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
 
 
   function createPost(newPost) {
     setPosts([...posts, newPost]);
     setModal(false);
+  }
+
+  async function fetchPosts() {
+    const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+    setPosts(response.data);
   }
 
   function removePost(post) {
